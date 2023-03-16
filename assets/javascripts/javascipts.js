14c77aed59d84d0e37e610fa9fda8d4e898ea2e7
h-pk-11 <105375102+h-pk-11@users.noreply.github.com>
2023-03-16 12:21:10 +0700

    Update slider in index.html, style.css, javascript.js

--- assets/javascripts/javascipts.js
+++ assets/javascripts/javascipts.js
@@ -1,28 +1,50 @@
 "use strict"
-// initial setup
-const container = document.querySelector('#container');
+// pad-size
 
-let r = document.querySelector(':root');
+const sliderValue = document.querySelector('.sliderValue');
+const slider = document.querySelector('input[type=range]');
 
-let rs = window.getComputedStyle(r);
+function setSlider(){
+    const maxVal = slider.max;
+    const minVal = slider.min;
 
-let n = rs.getPropertyValue('--resolution');
+    //calculate the percentage
+    const newVal = Number((slider.value - minVal)*100/(maxVal - minVal));
 
-for(let i = 1; i <= n*n; i++){
-    const item = document.createElement('div');
-    item.setAttribute('class',`item`);
-    item.setAttribute('id',`${i}`);
-    container.appendChild(item);
+    //calculate position of sliderValue div
+    const newPosition = 8 - (newVal * 0.16);
+
+    //update value on .sliderValue
+    sliderValue.innerHTML = `<span>${slider.value}</span>`;
+    sliderValue.style.left = `calc(${newVal}% + (${newPosition}px))`;
 }
+
+document.addEventListener('DOMContentLoaded',setSlider);
+
+slider.addEventListener('input', setSlider);
+
+
+// const container = document.querySelector('#container');
+
+// const inputBtn = document.querySelector('.slider');
+
+// inputBtn.addEventListener ('mouseup', changeResolution);
+
+// let root = document.documentElement;
+
+// function changeResolution(){
+//     root.style.setProperty('--resolution', n);
+    
+// }
+
+// for(let i = 1; i <= n*n; i++){
+//     const item = document.createElement('div');
+//     item.setAttribute('class',`item`);
+//     item.setAttribute('id',`${i}`);
+//     container.appendChild(item);
+// }
 
 // pad-size
 
-let btn_padSize = document.querySelector('#padSize');
-
-btn_padSize.addEventListener('click',changeResolution);
-
-function changeResolution(e){
-    prompt()
-}
 
 
